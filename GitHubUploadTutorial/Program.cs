2ba251f572c8d208690a87f516dcce3613e48af0
Ben Bush <bush108@mail.nmc.edu>
2020-09-28 10:52:09 -0400

    test

---
+++ GitHubUploadTutorial/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubUploadTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            age = DisplayGetUserAge();
+
+
+        }
+
+        static int DisplayGetUserAge()
+        {
+            int age;
+            string userResponse;
+            bool validResponse;
+
+
+            do
+            {
+                validResponse = true;
+
+                Console.Write("Hello human, how old are you?");
+                userResponse = Console.ReadLine();
+
+                if (!int.TryParse(userResponse, out age))
+                {
+                    Console.WriteLine("Please enter a proper age as a number.");
+                    validResponse = false;
+                }
+            } while (!validResponse);
+
+            return age;
+        }
+    }
+}
